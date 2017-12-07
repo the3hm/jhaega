24d2551ba2e1d8ce8f1525c7df59952326f2dee4
the3hm <the3hm@gmail.com>
2017-12-07 19:06:42 +0900

    This is the character web view push, mostly done to /web/ and /web/character

--- web/urls.py
+++ web/urls.py
@@ -10,9 +10,14 @@ from django.conf.urls import url, include
 from evennia.web.urls import urlpatterns
 
 # eventual custom patterns
+# custom_patterns = [
+#    # url(r'/desired/url/', view, name='example'),
+#]
+
 custom_patterns = [
-    # url(r'/desired/url/', view, name='example'),
-]
+    url(r'^character/', include('web.character.urls', namespace='character', app_name='character')),
+    ]
+
 
 # this is required by Django.
 urlpatterns = custom_patterns + urlpatterns
